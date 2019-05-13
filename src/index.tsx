c483a45d41d100d5920676b80640267b57713444
faisal <inifaisal.id@gmail.com>
2019-05-13 09:07:08 +0800

    add Card & Navbar component

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
 import './styles/styles.scss';
 
 export { default as Button } from './components/Button/Button';
+export { default as Card } from './components/Card/Card';
+export { default as Tabs } from './components/Tabs/Tabs';
+export { default as TabItem } from './components/Tabs/TabItem';
+export { default as Navbar } from './components/Navbar/Navbar';
+export { default as NavbarItem } from './components/Navbar/NavbarItem';
